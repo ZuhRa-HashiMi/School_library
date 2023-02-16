8282fdf4907c61f26a6f70e36e551cbff0ab4f98
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-16 17:18:01 +0430

    Add specialization into initialize

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,6 @@
 require_relative 'person'
 class Teacher < Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
