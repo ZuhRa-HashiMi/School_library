21a7148004d2b2cb71af5a56397fd8e3074fc5c3
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-16 15:53:42 +0430

    Solve the linter errors

--- person.rb
+++ person.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
-class Person 
-    def initialize(age, name = 'Unknown', parent_permission: true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
-# accessor get method
-attr_reader :id
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  # accessor get method
+  attr_reader :id
 
-# accessor get and set method
-attr_accessor :name, :age
+  # accessor get and set method
+  attr_accessor :name, :age
 
-#private method
-private
-def of_age?
+  # private method
+  private
+
+  def of_age?
     age >= 18
-end 
+  end
 
-#publick method
-def can_use_services?
+  # publick method
+  def can_use_services?
     of_age? || @parent_permission
-end
-
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,13 @@
 require_relative './person'
 
 class Teacher < Person
-    def initialize(age, name = 'Unknown', parent_permission: true)
-        super(age, name, parent_permission: parent_permission)
-        @specialization = specialization
-    end
-    attr_accessor :specialization
-    def can_use_services?
-        true
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+  attr_accessor :specialization
+
+  def can_use_services?
+    true
+  end
 end
