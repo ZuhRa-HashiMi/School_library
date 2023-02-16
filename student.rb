d4a4a603f85db28aca420d5d1c502c195b61a501
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-16 15:17:22 +0430

    Create student class

---
+++ student.rb
@@ -0,0 +1,9 @@
+require_relative './person'
+
+class Student < Person
+    def initialize(age, name = 'Unknown', parent_permission: true)
+        super(age, name, parent_permission: parent_permission)
+        @classroom = classroom
+    end
+
+end
