08540321e733af471b02eada5c9b462a73488502
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-16 14:25:14 +0430

    Initialize the instance variable

--- person.rb
+++ person.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
 class Person 
+    def initialize(age, name = 'Unknown', parent_permission: true)
+        @id = Random.rand(1..1000)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
 
 end
