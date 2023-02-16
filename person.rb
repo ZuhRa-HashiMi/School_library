4b03140b633279b8076a86c9753729d219458c74
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-17 00:50:24 +0430

    create person relative path

--- person.rb
+++ person.rb
@@ -27,3 +27,9 @@ class Person < Nameable
     @name
   end
 end
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name
