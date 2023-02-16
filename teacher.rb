4b9a0fda51b31f656998966ff1b6ee8c7b94a6e6
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-16 15:27:06 +0430

    Make the specialization true

--- teacher.rb
+++ teacher.rb
@@ -4,4 +4,9 @@ class Teacher < Person
     def initialize(age, name = 'Unknown', parent_permission: true)
         super(age, name, parent_permission: parent_permission)
         @specialization = specialization
+    end
+    attr_accessor :specialization
+    def can_use_services?
+        true
+    end
 end
