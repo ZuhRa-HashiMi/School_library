e9a55425b13a24a75d4b7881d7265dca89d9ac57
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-17 14:47:09 +0430

    Add classroom method

--- student.rb
+++ student.rb
@@ -5,6 +5,11 @@ class Student < person
     @classroom = classroom
   end
 
+  def classroom=(classroom)
+    @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?(self)
+  end
+
   def play_hooky?
     '¯\(ツ)/¯'
   end
