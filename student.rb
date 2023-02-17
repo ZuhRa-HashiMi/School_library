3aa9213bfcfda4cbe9334911ad2dcc3426de68af
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-17 16:11:47 +0430

    Add classroom methods

--- student.rb
+++ student.rb
@@ -1,16 +1,21 @@
-require_relative 'person'
-class Student < person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+require './person'
+
+class Student < Person
+  def initialize(age, name, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
-  def classroom=(classroom)
-    @classroom = classroom
-    @classroom.students.push(self) unless @classroom.students.include?(self)
+  attr_reader :classroom
+  attr_accessor :parent_permission
+
+  def play_hooky
+    "¯\(ツ)/¯"
   end
 
-  def play_hooky?
-    '¯\(ツ)/¯'
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
+
