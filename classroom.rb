fb0481fc30ae9dfb70121e18fd8606d66d7c609a
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-17 14:46:46 +0430

    Add new object

--- classroom.rb
+++ classroom.rb
@@ -1,16 +1,16 @@
-class Classroom 
-    attr_accessor :label, :students
+class Classroom
+  attr_accessor :label, :students
 
-    def initialize(label)
-        @label = label
-        @students = []
-    end
+  def initialize(label)
+    @label = label
+    @students = []
+  end
 
-    def add_studens(student)
-        @students.push(student)
-        student.classroom = self
-    end
+  def add_studens(student)
+    @students.push(student)
+    student.classroom = self
+  end
 end
-classes = Classroom.new("one")
+classes = Classroom.new('one')
 p classes.students
-p classes.add_studens("zuhra")
+p classes.add_studens('zuhra')
