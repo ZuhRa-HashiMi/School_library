50dd85e3ab0bbb6410ccc66d1fd2caf843199a4c
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-17 18:26:13 +0430

    solve the linter errors'

--- student.rb
+++ student.rb
@@ -8,6 +8,10 @@ class Student < Person
     @classroom = classroom
   end
 
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
