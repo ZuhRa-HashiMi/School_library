3286cb92a78899586c8a282a79246954657e4507
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-18 11:14:17 +0430

    Create the App class with thier method

---
+++ app.rb
@@ -0,0 +1,14 @@
+require_relative 'book'
+require_relative 'rental'
+require_relative 'student'
+require_relative 'teacher'
+
+class App 
+    def initialize 
+        @book = []
+        @people = []
+        @rental = []
+    end
+
+    
+end
