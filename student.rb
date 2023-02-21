3894a1c5f2f9b42de2677cea4f68460733e8746a
girma <girmatarekegn.gi@gmail.com>
2023-02-21 21:54:18 +0300

    Add to_json def in book rental and person

--- student.rb
+++ student.rb
@@ -16,4 +16,9 @@ class Student < Person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
+
+  def to_json(*_args)
+    { 'id' => @id, 'name' => @name, 'age' => @age,
+      'parent_permission' => @parent_permission, 'class' => self.class.name }
+  end
 end
